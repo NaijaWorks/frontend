85ad9954c14d0feb6eff4dbb7fd6926375b33594
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-12-07 18:44:26 +0000

    feat(frontend): add Text atoms

---
+++ src/~reusables/design-system/atoms/Text/Text.tsx
@@ -0,0 +1,66 @@
+// modules
+import { styled } from "../../../../contexts/ThemeContext";
+
+// components
+import { Text } from "../Primitives/Primitives";
+
+const Paragraph = styled(Text)`
+  line-height: ${props => props.theme.lineHeights.title};
+  font-size: ${props => props.theme.fontSizes[3]};
+`;
+
+Paragraph.defaultProps = {
+  color: "text"
+};
+
+export const P1 = styled(Paragraph)`
+  font-size: ${props => props.theme.fontSizes[4]};
+`;
+
+export const P2 = styled(Paragraph)`
+  font-size: ${props => props.theme.fontSizes[3]};
+`;
+
+export const P3 = styled(Paragraph)`
+  font-size: ${props => props.theme.fontSizes[2]};
+`;
+
+const Titles = styled(Text)``;
+Titles.defaultProps = { color: "title" };
+
+export const H1 = styled(Titles)`
+  line-height: ${props => props.theme.lineHeights.title};
+  font-size: ${props => props.theme.fontSizes[7]};
+  font-weight: ${props => props.theme.fontWeights[6]};
+`;
+
+export const H2 = styled(Titles)`
+  line-height: ${props => props.theme.lineHeights.title};
+  font-size: ${props => props.theme.fontSizes[6]};
+  font-weight: ${props => props.theme.fontWeights[6]};
+`;
+
+export const H3 = styled(Titles)`
+  line-height: ${props => props.theme.lineHeights.title};
+  font-size: ${props => props.theme.fontSizes[5]};
+  font-weight: ${props => props.theme.fontWeights[6]};
+`;
+
+export const H4 = styled(Titles)`
+  line-height: ${props => props.theme.lineHeights.copy};
+  font-size: ${props => props.theme.fontSizes[4]};
+  font-weight: ${props => props.theme.fontWeights[4]};
+`;
+
+export const H5 = styled(Titles)`
+  line-height: ${props => props.theme.lineHeights.copy};
+  font-size: ${props => props.theme.fontSizes[3]};
+  font-weight: ${props => props.theme.fontWeights[4]};
+`;
+
+export const H6 = styled(Titles)`
+  line-height: ${props => props.theme.lineHeights.copy};
+  font-size: ${props => props.theme.fontSizes[1]};
+  font-weight: ${props => props.theme.fontWeights[4]};
+  letter-spacing: ${props => props.theme.letterSpacing.tight};
+`;
---
+++ src/~reusables/design-system/molecules/NavHeader.tsx
@@ -0,0 +1,16 @@
+// modules
+import React from "react";
+
+// components
+import { Container } from "../atoms/Primitives/Primitives";
+import { P2 } from "../atoms/Text/Text";
+
+const NavHeader: React.FC = () => {
+  return (
+    <Container py={7}>
+      <P2>Hi</P2>
+    </Container>
+  );
+};
+
+export default NavHeader;
