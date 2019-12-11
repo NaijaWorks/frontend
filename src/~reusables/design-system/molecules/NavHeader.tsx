4b532dafeabc8675c146a1dbd6cf8b9e0e493595
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-12-11 21:03:52 +0000

    feat(frontend): add nav options, set up env variables

--- src/~reusables/design-system/molecules/NavHeader.tsx
+++ src/~reusables/design-system/molecules/NavHeader.tsx
@@ -1,13 +1,13 @@
 // modules
 import React, { useContext, useState } from "react";
 import { Link, withRouter, RouteComponentProps } from "react-router-dom";
-import { Search } from "react-feather";
+import { LogOut, Settings, UserCheck } from "react-feather";
 import { gql } from "apollo-boost";
-import { useMutation } from "@apollo/react-hooks";
+import { useMutation, useQuery } from "@apollo/react-hooks";
 
 // components
 import { Container, Flex, Box } from "../atoms/Primitives/Primitives";
-import { H3 } from "../atoms/Text/Text";
+import { H3, P2, H4 } from "../atoms/Text/Text";
 import { AuthContext, AuthData } from "../../../contexts/AuthContext";
 import { TextButton, PrimaryButton } from "../atoms/Button/Button";
 import PopupModal from "./PopupModal";
@@ -16,6 +16,7 @@ import { styled } from "../../../contexts/ThemeContext";
 
 // styles
 import { MAX_PAGE_WIDTH } from "../globals/metrics";
+import { GET_USER_INFO, UserInfo } from "../../../components/admin/Info";
 
 // mutation
 const LOGIN = gql`
@@ -53,6 +54,16 @@ const NavHeader: React.FC<OwnProps> = ({ history }) => {
       }
     }
   });
+
+  const { data } = useQuery<{ user: UserInfo }, { id: string }>(GET_USER_INFO, {
+    variables: { id: auth.id ? auth.id : "" }
+  });
+
+  const onClickLogout = () => {
+    localStorage.clear();
+    history.push("/");
+    window.location.reload();
+  };
 
   const onFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -102,15 +113,43 @@ const NavHeader: React.FC<OwnProps> = ({ history }) => {
         </Link>
         {auth.id ? (
           <Flex alignItems="center">
-            <Box bg="primary" borderRadius={5} p={4} mr={5}>
-              <Search width={16} height={16} color="white" />
-            </Box>
             <Box
-              bg="lightBackground"
+              className="log-out"
+              bg="primary"
               borderRadius={5}
-              width="36px"
-              height="36px"
-            ></Box>
+              p={4}
+              mr={5}
+              onClick={() => onClickLogout()}
+            >
+              <LogOut width={16} height={16} color="white" />
+            </Box>
+
+            <Link to={`/profile/${auth.id}`}>
+              <Flex
+                bg="lightBackground"
+                borderRadius={5}
+                width="36px"
+                height="36px"
+                justifyContent="center"
+                alignItems="center"
+              >
+                {data && data.user && data.user.photoURL ? (
+                  <img src={data.user.photoURL} alt="Profile" />
+                ) : data && data.user && data.user.firstName ? (
+                  <H4>
+                    {(data.user.firstName && data.user.firstName[0]) || ""}
+                    {(data.user.lastName && data.user.lastName[0]) || ""}
+                  </H4>
+                ) : (
+                  <UserCheck width={20} height={20} color="#000000" />
+                )}
+              </Flex>
+            </Link>
+            <Link to={`/admin`}>
+              <Box bg="primary" borderRadius={5} p={4} ml={5}>
+                <Settings width={16} height={16} color="white" />
+              </Box>
+            </Link>
           </Flex>
         ) : (
           <TextButton onClick={() => setModal(true)} color="primary">
@@ -125,6 +164,17 @@ const NavHeader: React.FC<OwnProps> = ({ history }) => {
 const StyledNavHeader = styled.div`
   padding: 0 ${props => props.theme.space[7]}px;
   background: ${props => props.theme.colors.background};
+  .log-out {
+    transform: scaleX(-1);
+    cursor: pointer;
+  }
+
+  img {
+    width: 100%;
+    height: 100%;
+    border-radius: 100%;
+    object-fit: cover;
+  }
 `;
 
 export default withRouter(NavHeader);
