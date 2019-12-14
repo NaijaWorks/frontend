dd30773c3b976f49b6048f19fd37cef47998b1a7
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-12-14 10:23:43 +0000

    feat(frontend): add apollo auth token on server contact

--- src/contexts/ApolloContext.tsx
+++ src/contexts/ApolloContext.tsx
@@ -1,9 +1,21 @@
 import React from "react";
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "@apollo/react-hooks";
+import { AuthData } from "./AuthContext";
 
 const client = new ApolloClient({
-  uri: `${process.env.REACT_APP_API_URL}/graphql`
+  uri: `${process.env.REACT_APP_API_URL}/graphql`,
+  request: operation => {
+    const auth: AuthData | null = JSON.parse(
+      localStorage.getItem("auth") as string
+    );
+
+    operation.setContext({
+      headers: {
+        authorization: auth ? auth.token : null
+      }
+    });
+  }
 });
 
 const ApolloContextProvider: React.FC = props => (
